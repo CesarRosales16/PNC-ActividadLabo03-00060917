de156d829bc2442e8857fb81c3f27cad2934f96e
Cesar Rosales <00060917@uca.edu.sv>
2020-05-01 17:32:29 -0600

    Response body 2

--- src/main/java/com/uca/capas/controller/MainController.java
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.uca.capas.domain.Student;
 
@@ -23,5 +25,19 @@ public class MainController {
 	public @ResponseBody List<Student> ejemplo2() {
 		return Arrays.asList(new Student("Nombre1", "Apellido1", "10/10/1992", "Carrera X", true),
 				new Student("Nombre2", "Apellido2", "10/10/1992", "Ingenieria en Limones", false));
+	}
+
+	@GetMapping("/inicio")
+	public String inicio(Student student) {
+		return "index";
+	}
+
+	@PostMapping("/formData")
+	public ModelAndView procesar(Student student) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("prueba");
+		mav.addObject("estado", student.delegateEstado());
+		mav.addObject("student", student);
+		return mav;
 	}
 }
--- src/main/java/com/uca/capas/domain/Student.java
+++ src/main/java/com/uca/capas/domain/Student.java
@@ -5,15 +5,15 @@ public class Student {
 	private String nombre;
 	private String apellido;
 	private String fEntrada;
-	private String carrea;
+	private String carrera;
 	private Boolean estado;
 
-	public Student(String nombre, String apellido, String fEntrada, String carrea, Boolean estado) {
+	public Student(String nombre, String apellido, String fEntrada, String carrera, Boolean estado) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.fEntrada = fEntrada;
-		this.carrea = carrea;
+		this.carrera = carrera;
 		this.estado = estado;
 	}
 
@@ -41,12 +41,12 @@ public class Student {
 		this.fEntrada = fEntrada;
 	}
 
-	public String getCarrea() {
-		return carrea;
+	public String getCarrera() {
+		return carrera;
 	}
 
-	public void setCarrea(String carrea) {
-		this.carrea = carrea;
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
 	}
 
 	public Boolean getEstado() {
@@ -55,5 +55,9 @@ public class Student {
 
 	public void setEstado(Boolean estado) {
 		this.estado = estado;
+	}
+
+	public String delegateEstado() {
+		return estado ? "Activo" : "Inactivo";
 	}
 }
